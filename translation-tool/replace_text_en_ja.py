dfda0ec96728727e70ec57c3e46b2cbe41ad2537
AyKo <neriyuzu+github@gmail.com>
2022-05-25 22:14:27 +0900

    翻訳対象を拡大、翻訳を少し改善。

--- translation-tool/replace_text_en_ja.py
+++ translation-tool/replace_text_en_ja.py
@@ -63,7 +63,7 @@ special_char_transtable_decode = str.maketrans({
     "👆":"™",  # upgraded
 })
 
-tag_list = ['.//text', './/desc', './/power', './/secretDescription']
+tag_list = ['.//text', './/desc', './/power', './/secretDescription', './/tooltip', './/visitedTooltip', './/unvisitedTooltip']
 
 translate_map = dict()
 translate_file = open(translate_filename, mode='r', encoding="utf8", errors='ignore')
